d00ccfe0cd0776b32b931318ad4ca78de4bb55cc
Samuel Mimram <smimram@gmail.com>
2026-05-06 17:36:25 +0200

    Add emacs mode.

---
+++ batt-mode.el
@@ -0,0 +1,38 @@
+;; batt-mode.el -- BATT major emacs mode
+
+(defvar batt-font-lock-keywords
+ '(
+   ("--.*" . 'font-lock-comment-face)
+   ("\\<\\(\\)\\>\\|:\\|=" . font-lock-keyword-face)
+   ("\\<\\(true\\|false\\|tt\\)\\>" . font-lock-constant-face)
+   ("\\<\\(U\\|Bool\\|Unit\\|Empty\\)\\>\\|->\\|→\\|⨂\\|⊗\\|@\\|≡\\|⊥\\|♭\\|𝄫" . font-lock-builtin-face)
+   ("\\<\\(\\)\\>" . font-lock-constant-face)
+   ("^\\([^ (=]*\\)" 1 'font-lock-function-name-face)
+  )
+)
+
+(defvar batt-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    ;; Allow some extra characters in words
+    (modify-syntax-entry ?_ "w" st)
+    ;; Comments
+    (modify-syntax-entry ?- ". 12" st)
+    (modify-syntax-entry ?\n ">" st)
+    st)
+  "Syntax table for BATT major mode.")
+
+(defvar batt-tab-width 4)
+
+(define-derived-mode batt-mode fundamental-mode
+  "BATT" "Major mode for BATT files."
+  :syntax-table batt-mode-syntax-table
+  (set (make-local-variable 'comment-start) "--")
+  (set (make-local-variable 'comment-start-skip) "--+\\s-*")
+  (set (make-local-variable 'font-lock-defaults) '(batt-font-lock-keywords))
+  (setq mode-name "BATT")
+)
+
+(provide 'batt-mode)
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.batt\\'" . batt-mode))
