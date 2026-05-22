9a018ad003641078f97d9ec8d52dcf1c5b9b77ba
Samuel Mimram <smimram@gmail.com>
2026-05-22 13:43:22 +0200

    Better handling of colors.

--- src/common.ml
+++ src/common.ml
@@ -3,15 +3,21 @@ let show_debug = ref true
 let debug fmt =
   if !show_debug then
     (
-      if !Terminal.enable_colors then print_string (Terminal.color 33);
-      Printf.kfprintf
-        (fun oc ->
-           if !Terminal.enable_colors then output_string oc (Terminal.color 0);
-           flush oc)
-        stdout fmt
+      print_string (Terminal.color `Yellow);
+      Printf.kfprintf (fun oc -> output_string oc (Terminal.color `Black); flush oc) stdout fmt
     )
   else Printf.ifprintf stdout fmt
 
-let important fmt = Printf.ksprintf (fun s -> print_string @@ Terminal.blue ~bold:true s) fmt
+let important fmt =
+  Printf.ksprintf (fun s ->
+      print_string (Terminal.color ~bold:true `Blue);
+      print_string s;
+      print_string (Terminal.color `Black)
+    ) fmt
 
-let error fmt = Printf.ksprintf (fun s -> print_string @@ Terminal.red ~bold:true s) fmt
+let error fmt =
+  Printf.ksprintf (fun s ->
+      print_string (Terminal.color ~bold:true `Red);
+      print_string s;
+      print_string (Terminal.color `Black)
+    ) fmt
