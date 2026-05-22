d41c33360915febc667e6875e4b288e0645f40bd
Samuel Mimram <smimram@gmail.com>
2026-05-22 12:52:33 +0200

    Drop when ignored.

--- src/common.ml
+++ src/common.ml
@@ -1,8 +1,16 @@
 let show_debug = ref true
 
 let debug fmt =
-  if !show_debug then Printf.ksprintf (fun s -> print_string @@ Terminal.yellow s) fmt
-  else Printf.ksprintf (fun _ -> ()) fmt
+  if !show_debug then
+    (
+      if !Terminal.enable_colors then print_string (Terminal.color 33);
+      Printf.kfprintf
+        (fun oc ->
+           if !Terminal.enable_colors then output_string oc (Terminal.color 0);
+           flush oc)
+        stdout fmt
+    )
+  else Printf.ifprintf stdout fmt
 
 let important fmt = Printf.ksprintf (fun s -> print_string @@ Terminal.blue ~bold:true s) fmt
 
