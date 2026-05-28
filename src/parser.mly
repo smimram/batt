a031a55c08de783058dac3478ebc3908ef02cab5
Samuel Mimram <smimram@gmail.com>
2026-05-28 09:38:43 +0200

    Modularize code.

--- src/parser.mly
+++ src/parser.mly
@@ -1,11 +1,11 @@
 %{
-open Lang
+open Term
 open Helper
 
 let binder_names ~pos t =
   let rec aux acc = function
-    | TVar x -> x :: acc
-    | TApp (t, _, TVar x) -> aux (x :: acc) t
+    | Var x -> x :: acc
+    | App (t, _, Var x) -> aux (x :: acc) t
     | _ -> failwith @@ Printf.sprintf "%s: binder expected" (Pos.to_string pos)
   in
   aux [] t
@@ -27,11 +27,9 @@ let binder_names ~pos t =
 %right TIMES
 %right TENSP
 %right TENS
-%nonassoc VAR
-%nonassoc IDENT
 
 %start main
-%type<Lang.decls> main
+%type<Term.decls> main
 %%
 
 main:
@@ -45,31 +43,31 @@ decls:
 
 decl:
   | x=IDENT c=ccolon a=term N def=def { let y, t = def in assert (x = y); (x, c, a, t) }
-  | POSTULATE x=IDENT c=ccolon a=term { (x, c, a, mk ~pos:$loc @@ TPostulate None) }
+  | POSTULATE x=IDENT c=ccolon a=term { (x, c, a, mk ~pos:$loc @@ Postulate None) }
 
 def:
   | y=IDENT args=list(pattern) EQ t=term { y, abss_pattern ~pos:$loc args t }
-  | y=IDENT args=list(pattern) LRPAR { y, abss_pattern ~pos:$loc args (mk ~pos:$loc($3)(TIndType_ind (`Empty, []))) }
+  | y=IDENT args=list(pattern) LRPAR { y, abss_pattern ~pos:$loc args (mk ~pos:$loc($3)(IndType_ind (`Empty, []))) }
 
 atom:
-  | TYPE { mk ~pos:$loc @@ TType }
-  | EMPTY { mk ~pos:$loc @@ TIndType `Empty }
-  | UNIT { mk ~pos:$loc @@ TIndType `Unit }
-  | TT { mk ~pos:$loc @@ TIndTerm `Unit }
-  | BOOL { mk ~pos:$loc @@ TIndType `Bool }
-  | FALSE { mk ~pos:$loc @@ TIndTerm (`Bool false) }
-  | BOOL_IND LPAR tf=fun_term COMMA tt=term RPAR { mk ~pos:$loc @@ TIndType_ind (`Bool, [tf;tt]) }
-  | TRUE { mk ~pos:$loc @@ TIndTerm (`Bool true) }
-  | IDENT %prec VAR { mk ~pos:$loc @@ TVar $1 }
-  | REFL { mk ~pos:$loc @@ TRefl }
-  | HOLE { mk ~pos:$loc @@ THole $loc }
-  | META { mk ~pos:$loc @@ TMeta (`Fresh (Some $loc)) }
+  | TYPE { mk ~pos:$loc @@ Type }
+  | EMPTY { mk ~pos:$loc @@ IndType `Empty }
+  | UNIT { mk ~pos:$loc @@ IndType `Unit }
+  | TT { mk ~pos:$loc @@ IndTerm `Unit }
+  | BOOL { mk ~pos:$loc @@ IndType `Bool }
+  | FALSE { mk ~pos:$loc @@ IndTerm (`Bool false) }
+  | BOOL_IND LPAR tf=fun_term COMMA tt=term RPAR { mk ~pos:$loc @@ IndType_ind (`Bool, [tf;tt]) }
+  | TRUE { mk ~pos:$loc @@ IndTerm (`Bool true) }
+  | IDENT { mk ~pos:$loc @@ Var $1 }
+  | REFL { mk ~pos:$loc @@ Refl }
+  | HOLE { mk ~pos:$loc @@ Hole $loc }
+  | META { mk ~pos:$loc @@ Meta (`Fresh (Some $loc)) }
   | LPAR t=term RPAR { t }
 
 prefix_term:
   | atom { $1 }
-  | FLAT t=prefix_term { mk ~pos:$loc @@ TFlat t }
-  | FLATTEN t=prefix_term { mk ~pos:$loc @@ TFlatten t }
+  | FLAT t=prefix_term { mk ~pos:$loc @@ Flat t }
+  | FLATTEN t=prefix_term { mk ~pos:$loc @@ Flatten t }
 
 app_term:
   | prefix_term { $1 }
@@ -78,23 +76,23 @@ app_term:
 
 prod_term:
   | app_term { $1 }
-  | a=prod_term TENS b=prod_term { mk ~pos:$loc @@ TTens (a, b) }
-  | a=prod_term TENSP b=prod_term { mk ~pos:$loc @@ TTensPair (a, b) }
-  | a=prod_term TIMES b=prod_term { mk ~pos:$loc @@ TSigma ("_", a, b) }
-  | t=prod_term IDEQ u=prod_term { mk ~pos:$loc @@ TEq (t, u) }
+  | a=prod_term TENS b=prod_term { mk ~pos:$loc @@ Tens (a, b) }
+  | a=prod_term TENSP b=prod_term { mk ~pos:$loc @@ TensPair (a, b) }
+  | a=prod_term TIMES b=prod_term { mk ~pos:$loc @@ Sigma ("_", a, b) }
+  | t=prod_term IDEQ u=prod_term { mk ~pos:$loc @@ Eq (t, u) }
 
 fun_term:
   | prod_term { $1 }
-  | a=prod_term TO s=option(dir) b=fun_term { match s with None -> mk ~pos:$loc @@ TPi (Explicit, Normal, "_", a, b) | Some s -> mk ~pos:$loc @@ TArr (s, a, b) }
+  | a=prod_term TO s=option(dir) b=fun_term { match s with None -> mk ~pos:$loc @@ Pi (Explicit, Normal, "_", a, b) | Some s -> mk ~pos:$loc @@ Arr (s, a, b) }
   | abs=nonempty_list(binder_group) TO b=fun_term { pis ~pos:$loc abs b }
-  | SIGMA LPAR x=IDENT COLON a=term RPAR DOT b=fun_term { mk ~pos:$loc @@ TSigma (x, a, b) }
+  | SIGMA LPAR x=IDENT COLON a=term RPAR DOT b=fun_term { mk ~pos:$loc @@ Sigma (x, a, b) }
   | FUN x=nonempty_list(pattern) to_dot t=fun_term { abss_pattern ~pos:$loc x t }
-  | LET x=IDENT c=ccolon a=term EQ t=term IN u=fun_term { mk ~pos:$loc @@ TLet (c, x, a, t, u) }
+  | LET x=IDENT c=ccolon a=term EQ t=term IN u=fun_term { mk ~pos:$loc @@ Let (c, x, a, t, u) }
   /* | LET x=pattern EQ t=term IN u=term { app (abs_pattern x u) t } */
 
 term:
   | t=fun_term { t }
-  | t=fun_term COMMA u=term { mk ~pos:$loc @@ TPair (t, u) }
+  | t=fun_term COMMA u=term { mk ~pos:$loc @@ Pair (t, u) }
 
 pattern:
   | x=identm d=option(dir) { `Var (Explicit,x,d) }
---
+++ src/value.ml
@@ -0,0 +1,215 @@
+type inductive_type = Term.inductive_type
+[@@deriving show]
+type inductive_term = Term.inductive_term
+[@@deriving show]
+type icit = Term.icit
+[@@deriving show]
+type crispness = Term.crispness
+[@@deriving show]
+type side = Term.side
+[@@deriving show]
+type term = Term.t
+type var = string
+[@@deriving show]
+
+(** A value. *)
+type t =
+  | Type
+  | IndType of inductive_type
+  | IndTerm of inductive_term
+  | IndType_ind of inductive_type * t list * spine
+  | Pi of icit * crispness * t * closure
+  | Abs of side option * closure
+  | Sigma of t * closure
+  | Pair of t * t
+  | Pair_ind of closure2 * spine
+  | Arr of side * t * t
+  | Tens of t * t
+  | TensPair of t * t
+  | Tens_ind of closure2 * spine
+  | Flat of t
+  | Flatten of t
+  | Flat_ind of closure * spine
+  | Eq of t * t
+  | Refl
+  | J of t * spine
+  | Meta of meta * spine
+  | Var of int * spine
+  | Hole of (Pos.t [@opaque]) * spine
+  | Postulate of int * spine
+[@@deriving show]
+
+(** A closure. *)
+and closure = var * (term[@opaque]) * (environment[@opaque])
+
+(** A binary closure. *)
+and closure2 = var * var * (term[@opaque]) * (environment[@opaque])
+
+(** An environment. *)
+and environment = (var * t) list
+
+(** A metavariable. *)
+and meta =
+  {
+    id : int;
+    pos : Pos.t option [@opaque];
+    mutable value : t option;
+  }
+
+(** A list of arguments: first in the list, last to be applied. *)
+and spine = t list
+
+(** Metavariables. *)
+module Meta = struct
+  type t = meta
+
+  (* Backward compatibility. *)
+  module Dynarray = struct
+    type 'a t = 'a array ref
+    let create () : 'a t = ref [||]
+    let length (a : 'a t) = Array.length !a
+    let to_list (a : 'a t) = Array.to_list !a
+    let add_last (a : 'a t) x = a := Array.append !a [|x|]
+    let get (a : 'a t) i = Array.get !a i
+  end
+
+  let variables = Dynarray.create ()
+
+  let to_string m = "?" ^ string_of_int m.id
+
+  (** Generate a fresh metavariable. *)
+  let fresh ?pos () =
+    let m = { id = Dynarray.length variables; pos; value = None } in
+    Dynarray.add_last variables m;
+    m
+
+  (** Get metavariable with given id. *)
+  let get id =
+    Dynarray.get variables id
+end
+
+(** Postulate counter. *)
+let postulate = ref (-1)
+
+(** Evaluate a term to a value. *)
+let rec eval (env:environment) : Term.t -> t = function
+  | Type -> Type
+  | IndType a -> IndType a
+  | IndTerm t -> IndTerm t
+  | IndType_ind (ind, args) -> IndType_ind (ind, List.map (eval env) args, [])
+  | Pi (i, c, x, a, t) -> Pi (i, c, eval env a, (x, t, env))
+  | Abs (_, s, x, t) -> Abs (s, (x, t, env))
+  | App (t, _, u) -> app (eval env t) (eval env u)
+  | Sigma (x, a, t) -> Sigma (eval env a, (x, t, env))
+  | Pair (t, u) -> Pair (eval env t, eval env u)
+  | Pair_ind (x, y, t) -> Pair_ind ((x, y, t, env), [])
+  | Arr (s, a, b) -> Arr (s, eval env a, eval env b)
+  | Tens (a, b) -> Tens (eval env a, eval env b)
+  | TensPair (t, u) -> TensPair (eval env t, eval env u)
+  | Tens_ind (x, y, t) -> Tens_ind ((x, y, t, env), [])
+  | Flat t -> Flat (eval env t)
+  | Flatten t -> Flatten (eval env t)
+  | Flat_ind (x, t) -> Flat_ind ((x, t, env), [])
+  | Eq (t, u) -> Eq (eval env t, eval env u)
+  | Refl -> Refl
+  | J r -> J (eval env r, [])
+  | Var x ->
+    (
+      match List.assoc_opt x env with
+      | Some v -> v
+      | None -> failwith @@ Printf.sprintf "eval: could not find %s" x
+    )
+  | Var' n -> snd @@ List.nth env n
+  | Let (_c,x,_a,t,u) ->
+    eval env (Term.app (Abs(Explicit, None, x, u)) t)
+  | Postulate (Some n) -> Postulate (n, [])
+  | Postulate None -> assert false
+  | Hole pos -> Hole (pos, [])
+  | Meta (`Fresh pos) -> fresh_meta ?pos env
+  | Meta (`Generated id) -> Meta (Meta.get id, [])
+
+(** Make a variable. *)
+and var k = Var (k, [])
+
+(** Generate a fresh metavariable. *)
+and fresh_meta ?pos env =
+  let m = Meta.fresh ?pos () in
+  (* We only keep variables in the environment. *)
+  let vars = List.filter_map (fun (_x,v) -> match force v with Var _ -> Some v | _ -> None) env in
+  Meta (m, vars)
+
+(** Apply a value to another. *)
+and app t u =
+  match force t, force u with
+  | Abs (_, f), u -> capp f u
+  | IndType_ind (`Unit, [t], []), IndTerm `Unit -> t
+  | IndType_ind (`Bool, [tf;_tt], []), IndTerm (`Bool false) -> tf
+  | IndType_ind (`Bool, [_tf;tt], []), IndTerm (`Bool true) -> tt
+  | IndType_ind (ind, t, l), u -> IndType_ind (ind, t, u::l)
+  | Pair_ind (t, []), Pair (u, v) -> capp2 t u v
+  | Pair_ind (t, l), u -> Pair_ind (t, u::l)
+  | Tens_ind (t, []), TensPair (u, v) -> capp2 t u v
+  | Tens_ind (t, l), u -> Tens_ind (t, u::l)
+  | Flat_ind (t, []), Flatten u -> capp t u
+  | Flat_ind (t, l), u -> Flat_ind (t, u::l)
+  | J (r, [_]), Refl -> r
+  | J (r, l), u -> J (r, u::l)
+  | Var (x, l), u -> Var (x, u::l)
+  | Meta (m, l), u -> Meta (m, u::l)
+  | Hole (pos, l), u -> Hole (pos, u::l)
+  | Postulate (n, l), u -> Postulate (n, u::l)
+  | _ -> failwith @@ Printf.sprintf "vapp: %s vs %s" (show t) (show u)
+
+(** Apply a value to a list of values. *)
+and apps t = function
+  | u::uu -> apps (app t u) uu
+  | [] -> t
+
+(** Apply a value to a spine of values. *)
+and app_spine t = function
+  | u::uu -> app (app_spine t uu) u
+  | [] -> t
+
+(** Instantiate a closure with a value. *)
+and capp ((x,t,env):closure) (v:t) =
+  eval ((x,v)::env) t
+
+and capp2 ((x,y,t,env):closure2) (u:t) (v:t) =
+  eval ((y,v)::(x,u)::env) t
+
+(** Remove already evaluated values. *)
+and force t =
+  match t with
+  | Meta (m, s) when m.value <> None -> app_spine (Option.get m.value) s
+  | _ -> t
+
+(** Reify a value as a term. *)
+let rec readback k v : Term.t =
+  let var_name k = "x" ^ string_of_int k in
+  let spine l t = Term.app_spine t (List.map (readback k) l) in
+  match force v with
+  | Type -> Type
+  | IndType ind -> IndType ind
+  | IndType_ind (ind, args, l) -> spine l @@ IndType_ind (ind, List.map (readback k) args)
+  | IndTerm t -> IndTerm t
+  | Pi (i, c, a, b) -> Pi (i, c, var_name k, readback k a, readback (k+1) (capp b (var k)))
+  | Abs (s, f) -> Abs (Explicit, s, var_name k, readback (k+1) (capp f (var k)))
+  | Sigma (a, b) -> Sigma (var_name k, readback k a, readback (k+1) (capp b (var k)))
+  | Pair (t, u) -> Pair (readback k t, readback k u)
+  | Pair_ind (t, l) -> spine l @@ Pair_ind (var_name k, var_name (k+1), readback (k+2) @@ capp2 t (var k) (var (k+1)))
+  | Arr (s, a, b) -> Arr (s, readback k a, readback k b)
+  | Tens (a, b) -> Tens (readback k a, readback k b)
+  | TensPair (t, u) -> TensPair (readback k t, readback k u)
+  | Tens_ind (t, l) -> spine l @@ Tens_ind (var_name k, var_name (k+1), readback (k+2) @@ capp2 t (var k) (var (k+1)))
+  | Flat a -> Flat (readback k a)
+  | Flatten t -> Flatten (readback k t)
+  | Flat_ind (t, l) -> spine l @@ Flat_ind (var_name k, readback (k+1) (capp t (var k)))
+  | Eq (t, u) -> Eq (readback k t, readback k u)
+  | Refl -> Refl
+  | J (r, l) -> spine l @@ J (readback k r)
+  | Meta (m, l) -> spine l @@ Meta (`Generated m.id)
+  | Var (i, l) -> spine l @@ Var' i
+  | Postulate (n, l) -> spine l @@ Postulate (Some n)
+  | Hole (pos, l) -> spine l @@ Hole pos
+
+let to_string k v = Term.to_string @@ readback k v
