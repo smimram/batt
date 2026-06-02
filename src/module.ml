9d52c5912877058527a798ddda24d10daaa57413
Samuel Mimram <smimram@gmail.com>
2026-06-02 11:24:04 +0200

    Preprocessor.

--- src/module.ml
+++ src/module.ml
@@ -4,7 +4,8 @@ let parse_file fname =
     (fun ic ->
        let lexbuf = Sedlexing.Utf8.from_channel ic in
        Sedlexing.set_filename lexbuf fname;
-       try MenhirLib.Convert.Simplified.traditional2revised Parser.main (Sedlexing.with_tokenizer Lexer.token lexbuf)
+       let token = Lexer.token |> Preprocessor.swap_open_include in
+       try MenhirLib.Convert.Simplified.traditional2revised Parser.main (Sedlexing.with_tokenizer token lexbuf)
        with
        | Failure err ->
          let pos = Sedlexing.lexing_positions lexbuf in
