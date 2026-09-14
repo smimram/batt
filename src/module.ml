80163bee0929e9c1955d826f215efa271e74f053
Samuel Mimram <smimram@gmail.com>
2026-09-14 13:54:40 +0200

    Starting to work.

--- src/module.ml
+++ src/module.ml
@@ -1,19 +1,25 @@
+let parse_lexbuf lexbuf =
+  try MenhirLib.Convert.Simplified.traditional2revised Parser.main (Sedlexing.with_tokenizer Lexer.token lexbuf)
+  with
+  | Failure err ->
+    let pos = Sedlexing.lexing_positions lexbuf in
+    let err = Printf.sprintf "Lexing error %s: %s" (Pos.to_string pos) err in
+    failwith err
+  | Parser.Error ->
+    let pos = Sedlexing.lexing_positions lexbuf in
+    let err = Printf.sprintf "Parsing error %s" (Pos.to_string pos) in
+    failwith err
+
+let parse_string s =
+  parse_lexbuf @@ Sedlexing.Utf8.from_string s
+
 (** Open module in given file. *)
 let parse_file fname =
   In_channel.with_open_bin fname
     (fun ic ->
        let lexbuf = Sedlexing.Utf8.from_channel ic in
        Sedlexing.set_filename lexbuf fname;
-       try MenhirLib.Convert.Simplified.traditional2revised Parser.main (Sedlexing.with_tokenizer Lexer.token lexbuf)
-       with
-       | Failure err ->
-         let pos = Sedlexing.lexing_positions lexbuf in
-         let err = Printf.sprintf "Lexing error %s: %s" (Pos.to_string pos) err in
-         failwith err
-       | Parser.Error ->
-         let pos = Sedlexing.lexing_positions lexbuf in
-         let err = Printf.sprintf "Parsing error %s" (Pos.to_string pos) in
-         failwith err
+       parse_lexbuf lexbuf
     )
 
 (** Open module with given name. *)
---
+++ web/batt.ml
@@ -0,0 +1,67 @@
+(** Interaction with a webpage. *)
+
+open Js_of_ocaml
+
+module Html = Dom_html
+
+let doc = Html.document
+
+let debug s = Console.console##debug (Js.string s)
+
+let loop s =
+  debug "Type checking code";
+  Lang.check_decls_toplevel @@ Module.parse_string s
+
+let number_of_int n = Js.number_of_float (float n)
+
+let run _ =
+  let jsget x = Js.Opt.get x (fun () -> assert false) in
+  let get_element_by_id id = doc##getElementById (Js.string id) |> jsget in
+  let input = get_element_by_id "input" |> Html.CoerceTo.textarea |> jsget in
+  let output = get_element_by_id "output" |> Html.CoerceTo.textarea |> jsget in
+  let send = get_element_by_id "send" |> Html.CoerceTo.button |> jsget in
+  let clear = get_element_by_id "clear" |> Html.CoerceTo.button |> jsget in
+
+  let print s =
+    let s = Js.to_string output##.value ^ s in
+    output##.value := Js.string s;
+    output##.scrollTop := number_of_int output##.scrollHeight
+  in
+  let error s =
+    print ("^(o.o)^ Error: " ^ s ^ "\n")
+  in
+  let read () =
+    Js.to_string input##.value
+  in
+  let do_send () =
+    output##.value := Js.string "";
+    try read () |> String.trim |> loop
+    with
+    | Failure e -> error e
+    | e -> error (Printexc.to_string e)
+  in
+  Common.print_string := print;
+
+  send##.onclick :=
+    Html.handler
+      (fun _ ->
+         do_send ();
+         Js.bool true
+      );
+  clear##.onclick :=
+    Html.handler
+      (fun _ ->
+         input##.value := Js.string "";
+         output##.value := Js.string "";
+         Js.bool true
+      );
+
+  input##focus;
+  input##select;
+
+  ignore (Js.Unsafe.eval_string "init();");
+
+  Js._false
+
+let () =
+  Html.window##.onload := Html.handler run
